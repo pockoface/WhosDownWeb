91732cc02227a083791a51f807b631c26fabde00
Chris Bama <chrisbama92@gmail.com>
2015-11-22 12:15:28 -0500

    Changed mail service to use SendGrid api

--- php/send_form_email.php
+++ php/send_form_email.php
@@ -1,137 +1,37 @@
 <?php
 
-if(isset($_POST['email'])) {
-
-
-
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-
-  $email_to = "yourdomain@domain.com";
-
-  $email_subject = "You Down?!";
-
-
-
-
-
-  function died($error) {
-
-        // your error code can go here
-
-    echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-
-    echo "These errors appear below.<br /><br />";
-
-    echo $error."<br /><br />";
-
-    echo "Please go back and fix these errors.<br /><br />";
-
-    die();
-
-  }
-
-
-
-    // validation expected data exists
-
-  if(!isset($_POST['name']) ||
-
-    !isset($_POST['email']) ||
-
-    !isset($_POST['message'])) {
-
-    died('We are sorry, but there appears to be a problem with the form you submitted.');       
-
-}
-
-
-
-    $name = $_POST['name']; // required
-
-    $email_from = $_POST['email']; // required
-
-    $message = $_POST['message']; // required
-
-
-
-    $error_message = "";
-
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-    if(!preg_match($email_exp,$email_from)) {
-
-      $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-
-    }
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-    if(!preg_match($string_exp,$name)) {
-
-      $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-
-    }
-
-    if(strlen($message) < 2) {
-
-      $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-
-    }
-
-    if(strlen($error_message) > 0) {
-
-      died($error_message);
-
-    }
-
-    $email_message = "Form details below.\n\n";
-
-
-
-    function clean_string($string) {
-
-      $bad = array("content-type","bcc:","to:","cc:","href");
-
-      return str_replace($bad,"",$string);
-
-    }
-
-
-
-    $email_message .= "Name: ".clean_string($name)."\n";
-
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-
-    $email_message .= "Message: ".clean_string($message)."\n";
-
-
-
-
-
-// create email headers
-
-    $headers = 'From: '.$email_from."\r\n".
-
-    'Reply-To: '.$email_from."\r\n" .
-
-    'X-Mailer: PHP/' . phpversion();
-
-    @mail($email_to, $email_subject, $email_message, $headers);  
-
-    ?>
-
-
-
-    <!-- include your own success html here -->
-
-
-
-    Thank you for contacting us. We will be in touch with you very soon.
-
-
-
-    <?php
-
-  }
-
-  ?>
+ $url = 'smtp.sendgrid.net';
+ $user = 'azure_1817263d3cfa53e07f693470e4ea6610@azure.com';
+ $pass = '8B8wooxbTaBwe0r'; 
+
+ $params = array(
+      'api_user' => $user,
+      'api_key' => $pass,
+      'to' => 'domain@yourdomain.com',
+      'subject' => 'You Down?!',
+      'html' => 'testing body',
+      'text' => 'testing body',
+      'from' => 'noreply@youdown.com',
+   );
+
+ $request = $url.'api/mail.send.json';
+
+ // Generate curl request
+ $session = curl_init($request);
+
+ // Tell curl to use HTTP POST
+ curl_setopt ($session, CURLOPT_POST, true);
+
+ // Tell curl that this is the body of the POST
+ curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+
+ // Tell curl not to return headers, but do return the response
+ curl_setopt($session, CURLOPT_HEADER, false);
+ curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+ // obtain response
+ $response = curl_exec($session);
+ curl_close($session);
+
+ // print everything out
+ print_r($response);
