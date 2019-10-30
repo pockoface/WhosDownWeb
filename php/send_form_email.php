f0d0e33864fb58ec7d5ac46a6fb3df6d744be646
jordrguez <30585093+jordrguez@users.noreply.github.com>
2019-10-29 22:15:33 -0400

    Update send_form_email.php

--- php/send_form_email.php
+++ php/send_form_email.php
@@ -32,11 +32,11 @@ $mail = new SendGrid\Email();
 
   $name = strip_tags(trim($_POST["name"]));
   $name = str_replace(array("\r","\n"),array(" "," "),$name);
-  $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+  $recipient_email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
   $message = trim($_POST["message"]);
 
   $email_content = "Name: $name\n";
-  $email_content .= "Email: $email\n\n";
+  $email_content .= "Email: $recipient_email\n\n";
   $email_content .= "Message:\n$message\n";
 
 try {
